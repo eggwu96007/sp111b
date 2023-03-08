2f9564a0da28310af82d231aa1ebe1e1c6554f75
吳柏翰 <eggwu@eggwuMacBook.local>
2023-03-08 16:20:24 +0800

    spWeek4

---
+++ week4/compiler.c
@@ -0,0 +1,155 @@
+#include <assert.h>
+#include "compiler.h"
+
+int E();
+void STMT();
+void IF();
+void BLOCK();
+
+int tempIdx = 0, labelIdx = 0;
+
+#define nextTemp() (tempIdx++)
+#define nextLabel() (labelIdx++)
+#define emit printf
+
+int isNext(char *set) {
+  char eset[SMAX], etoken[SMAX];
+  sprintf(eset, " %s ", set);
+  sprintf(etoken, " %s ", tokens[tokenIdx]);
+  return (tokenIdx < tokenTop && strstr(eset, etoken) != NULL);
+}
+
+int isEnd() {
+  return tokenIdx >= tokenTop;
+}
+
+char *next() {
+  // printf("token[%d]=%s\n", tokenIdx, tokens[tokenIdx]);
+  return tokens[tokenIdx++];
+}
+
+char *skip(char *set) {
+  if (isNext(set)) {
+    return next();
+  } else {
+    printf("skip(%s) got %s fail!\n", set, next());
+    assert(0);
+  }
+}
+
+// F = (E) | Number | Id
+int F() {
+  int f;
+  if (isNext("(")) { // '(' E ')'
+    next(); // (
+    f = E();
+    next(); // )
+  } else { // Number | Id
+    f = nextTemp();
+    char *item = next();
+    emit("t%d = %s\n", f, item);
+  }
+  return f;
+}
+
+// E = F (op E)*
+int E() {
+  int i1 = F();
+  while (isNext("+ - * / & | ! < > =")) {
+    char *op = next();
+    int i2 = E();
+    int i = nextTemp();
+    emit("t%d = t%d %s t%d\n", i, i1, op, i2);
+    i1 = i;
+  }
+  return i1;
+}
+
+// ASSIGN = id '=' E;
+void ASSIGN() {
+  char *id = next();
+  skip("=");
+  int e = E();
+  skip(";");
+  emit("%s = t%d\n", id, e);
+}
+
+// WHILE = while (E) STMT
+void WHILE() {
+  int whileBegin = nextLabel();
+  int whileEnd = nextLabel();
+  emit("(L%d)\n", whileBegin);
+  skip("while");
+  skip("(");
+  int e = E();
+  emit("if not t%d goto L%d\n", e, whileEnd);
+  skip(")");
+  STMT();
+  emit("goto L%d\n", whileBegin);
+  emit("(L%d)\n", whileEnd);
+}
+
+void DO(){
+  int whileBegin = nextLabel();
+  int whileEnd = nextLabel();
+  emit("(L%d)\n", whileBegin);
+  skip("do");
+  STMT();
+  skip("while");
+  skip("(");
+  int e = E();
+  emit("if not t%d goto L%d\n", e, whileEnd);
+  emit("(L%d)\n", whileEnd);
+  skip(")");
+  skip(";");
+  return ;
+ /* skip("while");
+  skip("(");
+  int e = E();
+  emit("if not t%d goto L%d\n", e, whileEnd);
+  skip(")");
+  STMT();
+  emit("goto L%d\n", whileBegin);
+  emit("(L%d)\n", whileEnd);*/
+//先做裡面的東西
+//判斷是否成立，如果成立就繼續做
+}
+
+// STMT = WHILE | BLOCK | ASSIGN
+void STMT() {
+  if (isNext("do"))
+    DO();
+  else if (isNext("while")) //如果token是while就執行
+    WHILE();
+  // else if (isNext("if"))
+  //   IF();
+  else if (isNext("{"))//如果token是{就執行
+    BLOCK();
+  else
+    ASSIGN();//如果都不是就代表是數字
+}
+
+// STMTS = STMT*
+void STMTS() {
+  while (!isEnd() && !isNext("}")) {
+    STMT();
+  }
+}
+
+// BLOCK = { STMTS }
+void BLOCK() {
+  skip("{");
+  STMTS();
+  skip("}");
+}
+
+// PROG = STMTS
+void PROG() {
+  STMTS();
+}
+
+void parse() {
+  printf("============ parse =============\n");
+  tokenIdx = 0;
+  PROG();
+}
